22fe8b4c60407f457999f91ded9100228f2292e9
Bruce Becker <brucellino@gmail.com>
2015-01-15 00:32:19 +0100

    Wonder if more than one logical check can be executed at once ?

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ mkdir -p $SOFT_DIR
 
 #  Download the source file
 
-if [[ ! -e $SRC_DIR/$SOURCE_FILE ]] ; then
+if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
   echo "seems like this is the first build - let's get the source"
   mkdir -p $SRC_DIR
   wget http://mirror.ufs.ac.za/gnu/gnu/$NAME/$NAME-$VERSION/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
